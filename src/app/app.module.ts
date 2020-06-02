9edfb4c878013422dda9817d5308fa0a8cf39acf
Carol Vasconcelos <cvsouza04@gmail.com>
2020-06-01 22:10:06 -0300

    Adjustments on App component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PoModule } from '@po-ui/ng-components';
 import { RouterModule } from '@angular/router';
+import { CharactersComponent } from './characters/characters.component';
+import { CharactersService } from './characters/characters.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent
   ],
   imports: [
     BrowserModule,
     PoModule,
+    HttpClientModule,
     RouterModule.forRoot([])
   ],
-  providers: [],
+  providers: [CharactersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
